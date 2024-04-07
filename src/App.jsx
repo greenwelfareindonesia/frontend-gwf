0b01b18d06900a63a99c85655d71e6a7aa4e125e
helmy <helmyfadlail.5@gmail.com>
2024-04-07 12:54:19 +0700

    fix: dashboard redesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { About, Events, Contact, GwfBandung, Home, Involved, Team, Workshop, Resource, Donate, Ecopedia } from "./pages";
-import {
-  DashboardPage,
-  ArticlePage,
-  ContactPage,
-  EcopediaPage,
-  EventsPage,
-  FeedbackPage,
-  GalleryPage,
-  VeganguidePage,
-  WorkshopPage,
-} from "./pages/dashboardPage";
 
 import AdminLogin from "./pages/adminloginpage/AdminLogin";
 
-import ProtectedAdmin from "./routes/ProtectedAdmin";
 import NotFound from "./pages/notFoundPage/NotFound";
-
-import VeganGuide from "./pages/veganguidepage/VeganGuide";
-import VeganDiet from "./pages/veganguidepage/VeganDiet";
-import FoodsToAvoid from "./pages/veganguidepage/FoodsToAvoid";
-import VeganSampleMenu from "./pages/veganguidepage/VeganSampleMenu";
-import FoodsToEat from "./pages/veganguidepage/FoodsToEat";
-import EnvironmentalBenefits from "./pages/veganguidepage/EnvironmentalBenefits";
-import TopTipsForVegan from "./pages/veganguidepage/TopTipsForVegan";
-import VeganDishesInIndo from "./pages/veganguidepage/VeganDishesInIndo";
-import VeganBooks from "./pages/veganguidepage/VeganBooks";
-import VeganMovies from "./pages/veganguidepage/VeganMovies";
-import HealthBenefits from "./pages/veganguidepage/HealthBenefits";
-import AnimalAgriculture from "./pages/veganguidepage/AnimalAgriculture";
+import Test from "./pages/dashboard/Test";
 
 const App = () => {
   return (
@@ -45,33 +21,9 @@ const App = () => {
         <Route path="/ecopedia" element={<Ecopedia />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/resource" element={<Resource />} />
-
-        <Route element={<ProtectedAdmin />}>
-          <Route path="/dashboard/article" element={<ArticlePage />} />
-          <Route path="/dashboard/contact" element={<ContactPage />} />
-          <Route path="/dashboard/ecopedia" element={<EcopediaPage />} />
-          <Route path="/dashboard/event" element={<EventsPage />} />
-          <Route path="/dashboard/feedback" element={<FeedbackPage />} />
-          <Route path="/dashboard/gallery" element={<GalleryPage />} />
-          <Route path="/dashboard/veganguide" element={<VeganguidePage />} />
-          <Route path="/dashboard/workshop" element={<WorkshopPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-        </Route>
+        <Route path="/test" element={<Test />} />
 
         <Route path="/login" element={<AdminLogin />} />
-
-        <Route path="/plantbasedhub" element={<VeganGuide />} />
-        <Route path="/vegandiet" element={<VeganDiet />} />
-        <Route path="/foodstoavoid" element={<FoodsToAvoid />} />
-        <Route path="/veganmenuforaweek" element={<VeganSampleMenu />} />
-        <Route path="/foodstoeat" element={<FoodsToEat />} />
-        <Route path="/environmentalbenefits" element={<EnvironmentalBenefits />} />
-        <Route path="/toptipsforgoingvegan" element={<TopTipsForVegan />} />
-        <Route path="/vegandishesinindo" element={<VeganDishesInIndo />} />
-        <Route path="/veganbooks" element={<VeganBooks />} />
-        <Route path="/veganmovies" element={<VeganMovies />} />
-        <Route path="/healthbenefits" element={<HealthBenefits />} />
-        <Route path="/animalagriculturefacts" element={<AnimalAgriculture />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
--- src/layouts/dashboard_section/Template.jsx
+++ src/layouts/dashboard_section/Template.jsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { HiArrowRightCircle } from "react-icons/hi2";
 import { RiArrowDownSLine } from "react-icons/ri";
 
-import { Link, useLocation } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 import {
   admin_user_icon,
@@ -35,9 +35,8 @@ const Menus = [
   { title: "Workshop", path: "/dashboard/workshop", src: workshop_icon },
 ];
 
-const DashboardSection = ({ children, titleField, buttonField, handlePopUp }) => {
+const DashboardSection = ({ children, titleField, buttonField }) => {
   const [open, setOpen] = React.useState(true);
-  const { pathname } = useLocation();
 
   const handleLogout = (e) => {
     e.preventDefault();
@@ -46,7 +45,7 @@ const DashboardSection = ({ children, titleField, buttonField, handlePopUp }) =>
   };
 
   return (
-    <section className="flex">
+    <section className="relative flex">
       <div
         className={` ${
           open ? "max-w-[240px] min-w-[230px]" : "max-w-[90px] min-w-[80px]"
@@ -61,16 +60,10 @@ const DashboardSection = ({ children, titleField, buttonField, handlePopUp }) =>
         <i className="h-0.5 w-full bg-light-1 absolute left-0"></i>
         <ul className="pt-4 space-y-2 ">
           {Menus.map((item, index) => (
-            <Link
-              key={index}
-              className={`flex  rounded-md p-2 cursor-pointer hover:bg-primary-1 text-gray-300 text-sm items-center gap-x-4 ${
-                item.path === pathname && "bg-primary-1"
-              } `}
-              to={item.path}
-            >
+            <NavLink key={index} className={({ isActive }) => (isActive ? "sidebar-menu bg-primary-1" : "sidebar-menu bg-primary-2")} to={item.path}>
               <Icon src={item.src} />
               <span className={`${!open && "hidden"} origin-left duration-200`}>{item.title}</span>
-            </Link>
+            </NavLink>
           ))}
         </ul>
       </div>
@@ -78,7 +71,7 @@ const DashboardSection = ({ children, titleField, buttonField, handlePopUp }) =>
         <div className="flex items-center justify-end h-16 px-8 bg-light-2">
           <div className="group !bg-light-2 relative p-2 flex items-center gap-1 cursor-pointer">
             <Icon src={admin_user_icon} />
-            <h1>{localStorage.getItem("name")}</h1>
+            <h1 className="text-sm">helmy fadlail</h1>
             <RiArrowDownSLine className="duration-300 group-hover:rotate-180" />
 
             <div className="absolute right-0 hidden p-4 shadow-lg top-10 group-hover:block z-1 bg-light-1 rounded-xl">
@@ -95,14 +88,10 @@ const DashboardSection = ({ children, titleField, buttonField, handlePopUp }) =>
         </div>
         <div className="mb-20 space-y-8">
           <div className="flex items-center justify-between px-4 pt-8">
-            <h1 className="text-3xl font-bold text-primary-1">{titleField}</h1>
-            {buttonField && handlePopUp && (
-              <Button onClick={handlePopUp} className="!text-xl !text-light-1 !bg-primary-2 hover:!bg-primary-1 rounded-lg">
-                {buttonField}
-              </Button>
-            )}
+            <h1 className="text-3xl font-bold text-primary-1">{titleField ?? null}</h1>
+            {buttonField ?? null}
           </div>
-          <div className="block">{children}</div>
+          <div className="block px-4">{children}</div>
         </div>
       </div>
     </section>
