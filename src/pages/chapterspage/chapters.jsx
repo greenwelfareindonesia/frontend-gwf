5ed36aa76ab87757d95448ca162953dc64e9b532
helmy <helmyfadlail.5@gmail.com>
2024-05-06 18:54:40 +0700

    styles: fix classname in chapters and workshop

--- src/pages/chapterspage/chapters.jsx
+++ src/pages/chapterspage/chapters.jsx
@@ -9,34 +9,34 @@ export default function Chapters() {
       <Navbar />
       {/* Hero Section */}
       <div className="items-center justify-center w-screen lg:grid lg:grid-cols-2">
-        <img src={HeroSectionImage} alt="Hero Section Image" className="w-full min-h-[300px] lg:hidden" />
+        <img src={HeroSectionImage} alt="Hero Section Image" className="w-full min-h-300 lg:hidden" />
         <div className="flex flex-col items-center h-full pt-28">
           <motion.h2
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 1.5 }}
             viewport={{ once: true }}
-            className="text-center  text-primary-1 text-xl lg:text-4xl mb-8"
+            className="mb-8 text-xl text-center text-primary-1 lg:text-4xl"
           >
-          Green Welfare Bandung
+            Green Welfare Bandung
           </motion.h2>
           <motion.h3
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 1.5 }}
             viewport={{ once: true }}
-            className="text-xl text-primary-1 font-Poppins text-center"
+            className="text-xl text-center text-primary-1 font-Poppins"
           >
-          COMING SOON
+            COMING SOON
           </motion.h3>
           <motion.h3
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 1.5 }}
             viewport={{ once: true }}
-            className="text-lg  text-primary-1 font-light italic text-center mb-9"
+            className="text-lg italic font-light text-center text-primary-1 mb-9"
           >
-          Site in process
+            Site in process
           </motion.h3>
           <motion.a
             href="https://www.instagram.com/greenwelfare.bdg/?igshid=MzRlODBiNWFlZA%3D%3D"
@@ -46,30 +46,27 @@ export default function Chapters() {
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 1.5 }}
             viewport={{ once: true }}
-            className="text-center font-Poppins-light mb-9 text-primary-1 underline"
+            className="text-center underline font-Poppins-light mb-9 text-primary-1"
           >
-          @greenwelfare.bdg
+            @greenwelfare.bdg
           </motion.a>
           <motion.button
             initial={{ opacity: 0, y: -20 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 1.5 }}
             viewport={{ once: true }}
-            className="bg-primary-1 lg:w-300 w-5/6 h-12 font-light text-light-1 text-sm rounded-none mb-8
-            hover:cursor-pointer hover:bg-opacity-70 ease-in-out transition duration-500 "
+            className="w-5/6 h-12 mb-8 text-sm font-light transition duration-500 ease-in-out rounded-none bg-primary-1 lg:w-300 text-light-1 hover:cursor-pointer hover:bg-opacity-70 "
           >
-          Donate to GWF Bandung
+            Donate to GWF Bandung
           </motion.button>
           <motion.button
             initial={{ opacity: 0, y: -20 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 1.5 }}
-            viewport={{ once: true }} 
-            className="lg:w-300 w-5/6 h-12 text-sm font-light text-primary-1 rounded-none border border-primary-1 
-            hover:cursor-pointer hover:bg-primary-1 hover:text-light-1 ease-in-out transition duration-500"
+            viewport={{ once: true }}
+            className="w-5/6 h-12 text-sm font-light transition duration-500 ease-in-out border rounded-none lg:w-300 text-primary-1 border-primary-1 hover:cursor-pointer hover:bg-primary-1 hover:text-light-1"
           >
-
-          Meet the People behind GWF Bandung
+            Meet the People behind GWF Bandung
           </motion.button>
         </div>
         <img src={HeroSectionImage} alt="Hero Section Image" className="hidden lg:inline-block min-h-[600px]" />
