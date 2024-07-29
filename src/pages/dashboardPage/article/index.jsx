b93a7d6078da667422a37900e41150a091f51b18
helmy <helmyfadlail.5@gmail.com>
2024-07-29 21:05:23 +0700

    refactor: clean code

--- src/pages/dashboardPage/article/index.jsx
+++ src/pages/dashboardPage/article/index.jsx
@@ -7,28 +7,28 @@ const Index = () => {
   const elemen = [
     {
       foto: Dummy1,
-      nama : 'Raihan Al',
-      email : 'Raihan@gmail.com',
-      topik : 'saya akan bekerjasama apakah ada kontak lebih lanjut?',
-      pesan : 'saya akan bekerjasama apakah ada kontak lebih lanjut?'
+      nama: "Raihan Al",
+      email: "Raihan@gmail.com",
+      topik: "saya akan bekerjasama apakah ada kontak lebih lanjut?",
+      pesan: "saya akan bekerjasama apakah ada kontak lebih lanjut?",
     },
     {
       foto: Dummy2,
-      nama : 'Faza Ramadhan',
-      email : 'Faza@gmail.com',
-      topik : 'saya akan bekerjasama apakah ada kontak lebih lanjut?',
-      pesan : 'saya akan bekerjasama apakah ada kontak lebih lanjut?'
+      nama: "Faza Ramadhan",
+      email: "Faza@gmail.com",
+      topik: "saya akan bekerjasama apakah ada kontak lebih lanjut?",
+      pesan: "saya akan bekerjasama apakah ada kontak lebih lanjut?",
     },
   ];
 
   return (
     <Sidebar titleField={"Article"}>
-      <div className="flex absolute top-24 right-9 justify-between items-center mb-4">
+      <div className="absolute flex items-center justify-between mb-4 top-24 right-9">
         <Link to="/post3">
-          <button className="bg-primary-2 w-36 h-9 rounded-3xl font-semibold text-light-1">Post Article</button>
+          <button className="font-semibold bg-primary-2 w-36 h-9 rounded-3xl text-light-1">Post Article</button>
         </Link>
       </div>
-      <table className="w-full table-fixed text-left text-primary-2">
+      <table className="w-full text-left table-fixed text-primary-2">
         <thead>
           <tr className="border-b-1 border-t-1 border-primary-2">
             <th className="pt-3.5 pb-3.5 w-48">Foto</th>
@@ -43,16 +43,12 @@ const Index = () => {
             <tr key={index} className="border-b-1 border-primary-2">
               <td className="pt-5 pb-5 w-[217px] h-[111px]">
                 <div className="container w-[217px] h-[111px] overflow-hidden">
-                  <img 
-                    src={elemens.foto} 
-                    alt="Gambar Workshop" 
-                    className="object-cover w-full h-full" 
-                  />
+                  <img src={elemens.foto} alt="Gambar Workshop" className="object-cover w-full h-full" />
                 </div>
               </td>
               <td className="pt-3.5 pb-3.5">
                 <div className="container w-64 overflow-hidden">
-                  <p className=" text-sm align-text-top">{elemens.nama}</p>
+                  <p className="text-sm align-text-top ">{elemens.nama}</p>
                 </div>
               </td>
               <td className="pt-3.5 pb-3.5">
--- src/pages/dashboardPage/article/post.jsx
+++ src/pages/dashboardPage/article/post.jsx
@@ -1,54 +1,59 @@
 // import { Link } from "react-router-dom";
-import {camera_icon} from "../../../assets/icons";
+import { camera_icon } from "../../../assets/icons";
 import Sidebar from "../../../layouts/dashboard_section/Template";
 
 const post = () => {
-  return(
+  return (
     <section className="bg-white">
+      <Sidebar>
+        <div className="container fixed inset-0 flex items-center w-auto bg-white ml-60">
+          <div className="w-full p-10">
+            <div className="flex">
+              <h1 className="flex-1 pb-5 text-3xl font-semibold text-primary-2">Add Post</h1>
+            </div>
 
-    <Sidebar>
-      <div className="fixed inset-0 container w-auto ml-60 bg-white flex items-center">
-        <div className="p-10 w-full">
-          <div className="flex">
-            <h1 className="text-3xl text-primary-2 pb-5 font-semibold flex-1">Add Post</h1>
-          </div>
+            {/* nama  */}
+            <p className="py-2">Nama</p>
+            <input className="w-full h-8 rounded-md border-primary-1 border-1" type="text" placeholder="  tulis nama disini"></input>
 
-          {/* nama  */}
-          <p className="py-2">Nama</p>
-          <input className="border-primary-1 rounded-md h-8 w-full border-1" type="text" placeholder="  tulis nama disini"></input>
+            {/* email  */}
+            <p className="py-2">Email</p>
+            <input className="w-full h-8 rounded-md border-primary-1 border-1" type="text" placeholder="  tulis email disini"></input>
 
-          {/* demail  */}
-          <p className="py-2">Email</p>
-          <input className="border-primary-1 rounded-md h-8 w-full border-1" type="text" placeholder="  tulis email disini"></input>
+            {/* Foto */}
+            <div className="container flex">
+              <div className="flex flex-col flex-1 py-2">
+                <p className="my-5">Add Photo</p>
+                <label
+                  htmlFor="photo-upload"
+                  className="flex flex-col items-center justify-center p-4 my-2 rounded-md cursor-pointer w-60 border-1 border-primary-2 h-36"
+                >
+                  <div className="text-center">
+                    {" "}
+                    <img src={camera_icon} className="duration-75 hover:scale-150"></img>
+                  </div>
+                  <input id="photo-upload" type="file" className="hidden" />
+                </label>
+              </div>
+            </div>
 
-          {/* Foto */}
-          <div className="container flex">
-            <div className="flex-1 flex flex-col py-2">
-              <p className="my-5">Add Photo</p>
-              <label htmlFor="photo-upload" className="flex flex-col items-center justify-center w-60 border-1 border-primary-2 p-4 my-2 h-36 rounded-md cursor-pointer">
-                <div className="text-center"> <img src={camera_icon} className="hover:scale-150 duration-75"></img></div>
-                <input id="photo-upload" type="file" className="hidden" />
-              </label>
+            {/* topik  */}
+            <p className="py-2">Topik</p>
+            <input className="w-full h-8 rounded-md border-primary-1 border-1" type="text" placeholder="  tulis topik disini"></input>
+
+            {/* pesan artikel  */}
+            <p className="py-2">Pesan Artikel</p>
+            <input className="w-full h-8 rounded-md border-primary-1 border-1" type="text" placeholder="  tulis pesan artikel disini"></input>
+
+            {/* button untuk finalisasi post */}
+            <div>
+              <button className="w-full h-8 mt-4 text-xs text-white rounded-md bg-primary-2">Post Event</button>
             </div>
           </div>
-
-          {/* topik  */}
-          <p className="py-2">Topik</p>
-          <input className="border-primary-1 rounded-md h-8 w-full border-1" type="text" placeholder="  tulis topik disini"></input>
-
-          {/* pesan artikel  */}
-          <p className="py-2">Pesan Artikel</p>
-          <input className="border-primary-1 rounded-md h-8 w-full border-1" type="text" placeholder="  tulis pesan artikel disini"></input>
-
-          {/* button untuk finalisasi post */}
-          <div>
-            <button className="mt-4 h-8 bg-primary-2 text-white w-full rounded-md text-xs">Post Event</button>
-          </div>
         </div>
-      </div>
-    </Sidebar>
+      </Sidebar>
     </section>
-    );
-  };
+  );
+};
 
 export default post;
